83794053aed83a9b0caeac94cecb335379e87991
Albert Chung <albertswchung@gmail.com>
2022-05-03 23:07:39 +0100

    Add project dependencies, move test out.

--- gi_tract.py
+++ gi_tract.py
@@ -1,5 +1,4 @@
 import cv2
-import pytest
 import torch
 import torch.nn as nn
 import shutil
@@ -215,19 +214,6 @@ class UNet(nn.Module):
         x = self.softmax(x)
 
         return x
-
-
-@pytest.mark.parametrize("filters", [[32] * i for i in range(1, 6)])
-def test_unet(filters):
-    channels = 1
-    num_classes = 3
-
-    model = UNet(filters, channels, num_classes, kernel_size=(3, 3))
-
-    x = torch.ones((1, 1, 512, 512))
-    y = model(x)
-
-    assert y.shape == (1, 3, 512, 512)
 
 
 def main():
---
+++ tests/test_unet.py
@@ -0,0 +1,17 @@
+import pytest
+import torch
+
+from gi_tract import UNet
+
+
+@pytest.mark.parametrize("filters", [[32] * i for i in range(1, 6)])
+def test_unet(filters):
+    channels = 1
+    num_classes = 3
+
+    model = UNet(filters, channels, num_classes, kernel_size=(3, 3))
+
+    x = torch.ones((1, 1, 512, 512))
+    y = model(x)
+
+    assert y.shape == (1, 3, 512, 512)

7d689b72961cea219eaf2b52ed282f471895fcbc
Albert Chung <albertswchung@gmail.com>
2022-05-05 08:32:06 +0100

    Figure out class distribution.

---
+++ eda/class_distributions.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+
+def image_class_distribution():
+    df = pd.read_csv("train.csv")
+    df["values"] = np.where(df["segmentation"].isna(), 0, 1)
+    summary = df.pivot(index=["id"], columns=["class"], values=["values"])
+    num_images = len(df)
+    class_counts = summary.sum(axis=0) / num_images
+
+    return class_counts
+
+
+if __name__ == "__main__":
+    counts = image_class_distribution()
+    print(counts)

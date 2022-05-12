d02f3ef6490f1152201972f597468105cf59f532
Albert Chung <albertswchung@gmail.com>
2022-05-12 21:58:22 +0100

    Implement image histogram equalization to convert u16 to u8.

--- eda/visualisations.py
+++ eda/visualisations.py
@@ -1,4 +1,3 @@
-import cv2
 import random
 import numpy as np
 import plotly.graph_objects as go
@@ -8,19 +7,33 @@ from PIL import Image, ImageDraw
 from plotly.subplots import make_subplots
 
 
+def image_histogram_equalization(image: np.ndarray) -> np.ndarray:
+    max_u16 = 65536
+    hist, _ = np.histogram(image.flatten(), max_u16, [0, max_u16])
+    cdf = hist.cumsum()
+
+    cdf_m = np.ma.masked_equal(cdf, 0)
+    cdf_m = (cdf_m - cdf_m.min()) * max_u16 / (cdf_m.max() - cdf_m.min())
+    cdf = np.ma.filled(cdf_m, 0).astype(np.uint16)
+
+    image = cdf[image]
+    image = (image / 256).astype(np.uint8)
+    return image
+
+
 def create_color_mask(image: np.ndarray, label: np.ndarray) -> np.ndarray:
     classes = {
-        0: (255, 0, 0, 128),
-        1: (0, 255, 0, 128),
-        2: (0, 0, 255, 128),
+        1: (255, 0, 0, 128),
+        2: (0, 255, 0, 128),
+        3: (0, 0, 255, 128),
     }
 
+    image = image_histogram_equalization(image)
     image = Image.fromarray(image).convert("RGBA")
     overlay = Image.new("RGBA", image.size, (255, 255, 255, 0))
     drawing = ImageDraw.Draw(overlay)
-
     for cls, color in classes.items():
-        mask = label[..., cls] * 255
+        mask = np.array(label[..., cls] * 255, np.uint8)
         mask = Image.fromarray(mask, mode="L")
         drawing.bitmap((0, 0), mask, color)
 
@@ -29,14 +42,14 @@ def create_color_mask(image: np.ndarray, label: np.ndarray) -> np.ndarray:
 
 
 def visualise_random_images(images_path: Path, num_images: int):
-    all_images = list(images_path.rglob("*.png"))
+    all_images = list(images_path.rglob("*.npy"))
 
     image_paths = random.sample(all_images, num_images)
     label_paths = [Path(str(img).replace("images", "labels")) for img in image_paths]
     color_masks = [
         create_color_mask(
-            cv2.imread(str(ip), cv2.IMREAD_UNCHANGED),
-            cv2.imread(str(lp), cv2.IMREAD_UNCHANGED),
+            np.load(ip),
+            np.load(lp),
         ) for (ip, lp) in zip(image_paths, label_paths)
     ]
     cols = 4
@@ -51,3 +64,13 @@ def visualise_random_images(images_path: Path, num_images: int):
             fig.add_trace(go.Image(z=color_masks[i * cols + j]), row=i+1, col=j+1)
 
     fig.show()
+
+
+def main():
+    images_path = Path("dataset", "images")
+    num_images = 16
+    visualise_random_images(images_path, num_images)
+
+
+if __name__ == "__main__":
+    main()

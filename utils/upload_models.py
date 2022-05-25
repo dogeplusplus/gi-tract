2f98a388cd964deb3ac9bef7b13a4c73c04e89d4
Albert Chung <albertswchung@gmail.com>
2022-05-25 20:12:36 +0100

    Extend model upload to support model ensembles as model folders.

--- utils/upload_models.py
+++ utils/upload_models.py
@@ -1,6 +1,7 @@
 import shutil
 import logging
 import subprocess
+import typing as t
 
 from pathlib import Path
 from argparse import ArgumentParser
@@ -10,11 +11,17 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def commit_model(model_run: str, mlruns_folder: Path, dataset_dir: Path) -> int:
-    model_path = mlruns_folder / model_run / "artifacts/model/data/model.pth"
-    last_version = [int(x.stem[1:]) for x in dataset_dir.rglob("*.pth")]
+def commit_model(model_runs: t.List[str], mlruns_folder: Path, dataset_dir: Path) -> int:
+    last_version = [int(x.stem) for x in dataset_dir.rglob("*/**")]
     version = max(last_version) + 1
-    shutil.copyfile(model_path, dataset_dir / f"v{version}.pth")
+
+    model_dir = dataset_dir / str(version)
+    model_dir.mkdir()
+
+    for i, run in enumerate(model_runs):
+        model_path = mlruns_folder / run / "artifacts/model/data/model.pth"
+        shutil.copyfile(model_path, model_dir / f"model_{i}.pth")
+
     command = [
         "kaggle",
         "datasets",
@@ -40,15 +47,16 @@ def parse_arguments():
         help="Path to model dataset directory",
         default="model_instances",
     )
-    parser.add_argument("--run", type=str, help="Id of the model run")
+    parser.add_argument("--runs", type=str, nargs="+", help="Id of the model run")
+    parser.add_argument("--experiment", type=str, help="Experiment ID", default=0)
 
     args = parser.parse_args()
     return args
 
 
 def main(args):
-    mlruns_folder = Path("mlruns/0")
-    return_code = commit_model(args.run, Path(mlruns_folder), Path(args.directory))
+    mlruns_folder = Path("mlruns") / str(args.experiment)
+    return_code = commit_model(args.runs, Path(mlruns_folder), Path(args.directory))
     if return_code == 0:
         logger.info("Successfully created new dataset on kaggle.")
     else:

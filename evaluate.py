a0a77584bbe3376090e30beff7d5688554bce882
Albert Chung <albertswchung@gmail.com>
2022-05-27 21:51:21 +0100

    Add offline model evaluation script.

---
+++ evaluate.py
@@ -0,0 +1,70 @@
+import torch
+import logging
+import torch.nn as nn
+import monai.transforms as transforms
+
+from tqdm import tqdm
+from pathlib import Path
+from utils.dataset import GITract
+from argparse import ArgumentParser
+from torchmetrics import MeanMetric
+from torch.utils.data import DataLoader
+
+from train import dice_coef, iou_coef
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def parse_arguments():
+    parser = ArgumentParser("Evaluate model performance")
+    parser.add_argument("--model", type=str, help="Path to model weights.")
+    parser.add_argument("--dataset", type=str, help="Path to dataset.")
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_arguments()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = torch.load(args.model, map_location=device)
+    model.eval()
+
+    images_dir = Path(args.dataset) / "images"
+    images = list(images_dir.rglob("*.npy"))
+    labels = [Path(str(p).replace("images", "labels")) for p in images]
+
+    preprocessing = transforms.Compose([
+        transforms.Resized(keys=["image", "label"], spatial_size=(224, 224)),
+    ])
+    dataset = GITract(images, labels, preprocessing)
+    batch_size = 32
+
+    loader = DataLoader(dataset, batch_size)
+
+    thr = 0.5
+
+    mean_dice = MeanMetric().to(device)
+    miou = MeanMetric().to(device)
+
+    for xs, ys in tqdm(loader, total=len(loader)):
+        xs = xs.to(device)
+        ys = ys.to(device)
+        pred = model(xs)
+        pred = nn.Sigmoid()(pred)
+        pred = pred > thr
+
+        dice = dice_coef(ys, pred)
+        iou = iou_coef(ys, pred)
+
+        mean_dice.update(dice)
+        miou.update(iou)
+
+    logger.info(f"Mean Dice: {mean_dice.compute().cpu().numpy()}")
+    logger.info(f"Mean IoU: {miou.compute().cpu().numpy()}")
+
+
+if __name__ == "__main__":
+    main()
